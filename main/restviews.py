65d066c2b5351c2a33ce8ff28aa6135c67d3ee04
Alex <alex_govr@mail.ru>
2021-01-30 15:15:19 +0300

    upd: add selection viewset

--- main/restviews.py
+++ main/restviews.py
@@ -1,10 +1,27 @@
 
 from rest_framework import viewsets
 from rest_framework.response import Response
+from rest_framework.decorators import action
 from .models import Manufacturer, EqMark, EqModel, EqType
 from .serializers import ManufacturerSerializer, EqModelSerializer
 from .serializers import TypeSerializer, MarkSerializer
+from .plots import choose_pumps
 
+
+class SelectViewSet(viewsets.ViewSet):
+    queryset = EqMark.objects.none()
+
+    @action(methods=['post'], detail=True, url_path='/post/')
+    def select(request):
+        data = request.data
+        wpq = data['wpq']
+        wph = data['wph']
+        eqtype_name = data['eqtype']
+        marks = EqMark.objects.filter(eqtype=EqType.objects.get(eqtype=eqtype_name))
+        choosen = choose_pumps(marks, (wpq, wph))
+        srl = MarkSerializer(choosen, many=True)
+        print(srl.validated_data)
+        return Response(srl.data)
 
 class ManufacturerViewSet(viewsets.ModelViewSet):
     queryset = Manufacturer.objects.all()
