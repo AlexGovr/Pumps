60001407fa8d99bae58b6ed77681815fc46adc4e
Alex <alex_govr@mail.ru>
2021-01-30 12:29:44 +0300

    upd: select: add ajax

--- main/static/main/main.js
+++ main/static/main/main.js
@@ -8,38 +8,94 @@ function set_visible(item_class) {
     document.querySelector(`.${item_class}`).hidden = false;
   };
 
+// csrf_token 
+function getCookie(name) {
+  var cookieValue = null;
+  var i = 0;
+  if (document.cookie && document.cookie !== '') {
+      var cookies = document.cookie.split(';');
+      for (i; i < cookies.length; i++) {
+          var cookie = jQuery.trim(cookies[i]);
+          // Does this cookie string begin with the name we want?
+          if (cookie.substring(0, name.length + 1) === (name + '=')) {
+              cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+              break;
+          }
+      }
+  }
+  return cookieValue;
+}
+
 function table_add(table) {
-let row_num = table.children.length - 1;
-let row_template = document.querySelector('.select-table-row-template');
-let row = row_template.cloneNode();
-// let col_template = document.querySelector('.select-table-col-template');
-let scope = document.querySelector('.select-table-row-scope').cloneNode();
-let colnum = row_template.children.length - 1;
+  let row_num = table.children.length - 1;
+  let row_template = document.querySelector('.select-table-row-template');
+  let row = row_template.cloneNode();
+  // let col_template = document.querySelector('.select-table-col-template');
+  let scope = document.querySelector('.select-table-row-scope').cloneNode();
+  let colnum = row_template.children.length - 1;
 
-table.append(row)
+  table.append(row)
 
-row.hidden = false;
-row.append(scope);
+  row.hidden = false;
+  row.append(scope);
 
-scope.innerHTML = row_num+1;
-for (let i=0; i<colnum; i++) {
+  scope.innerHTML = row_num+1;
+  for (let i=0; i<colnum; i++) {
     let col = document.createElement('td');
     col.innerHTML = '-'
     row.appendChild(col)
-}
+  }
 }
 
 addEventListener(
 "DOMContentLoaded",
 function() {
-    // switch modes
-    var table = document.querySelector('.select-table-rows');
-    var page_items = document.querySelectorAll('.page-item');
-    for (let i = 0; i < page_items.length; i++) {
+  //// switcher modes
+  var table = document.querySelector('.select-table-rows');
+  var page_items = document.querySelectorAll('.page-item');
+  for (let i = 0; i < page_items.length; i++) {
     page_items[i].onclick = function() {
-        set_visible(this.dataset.page);             
-        table_add(table);
+      set_visible(this.dataset.page);             
+      table_add(table);
     }
+  }
+
+  //// form ajax button
+  document.querySelector('.select-parameters-send').onclick = function() {
+
+    let csrftoken = getCookie('csrftoken');
+
+    request = new XMLHttpRequest();
+    request.open('POST', '/select/');
+    request.setRequestHeader('X-CSRFToken', csrftoken);
+
+    // retrieve data to json
+    let wpq = document.querySelector('.select-parameters-q').value;
+    let wph = document.querySelector('.select-parameters-h').value;
+    let datajson = {'wpq': wpq, 'wph': wph};
+
+    for (let i = 0; i < tr_exercises.length; i++) {
+        let trex_id = tr_exercises[i].dataset.tr_exercise_id;
+        let mark = tr_exercises[i].getElementsByClassName(`${mark_class}`)[0];
+        let estim = tr_exercises[i].getElementsByClassName(`${estim_class}`)[0];
+        datajson[trex_id] = [mark.value, estim.value];
     }
+
+    request.onload = function() {
+        const data = JSON.parse(request.responseText)
+        if (data.success) {
+            console.log('success');
+        }
+    }
+
+    data = new FormData();
+
+    data.append('marks', JSON.stringify(datajson));
+    request.send(data);
+
+    return false;
+}
+}
+
 }
 );
