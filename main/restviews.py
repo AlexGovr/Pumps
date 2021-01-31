b88360161bd8a43f0527725740762b97486b9ddf
Alex <alex_govr@mail.ru>
2021-01-31 09:41:21 +0300

    upd: selection api view

--- main/restviews.py
+++ main/restviews.py
@@ -1,4 +1,4 @@
-
+import json
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.decorators import action
@@ -7,21 +7,6 @@ from .serializers import ManufacturerSerializer, EqModelSerializer
 from .serializers import TypeSerializer, MarkSerializer
 from .plots import choose_pumps
 
-
-class SelectViewSet(viewsets.ViewSet):
-    queryset = EqMark.objects.none()
-
-    @action(methods=['post'], detail=True, url_path='/post/')
-    def select(request):
-        data = request.data
-        wpq = data['wpq']
-        wph = data['wph']
-        eqtype_name = data['eqtype']
-        marks = EqMark.objects.filter(eqtype=EqType.objects.get(eqtype=eqtype_name))
-        choosen = choose_pumps(marks, (wpq, wph))
-        srl = MarkSerializer(choosen, many=True)
-        print(srl.validated_data)
-        return Response(srl.data)
 
 class ManufacturerViewSet(viewsets.ModelViewSet):
     queryset = Manufacturer.objects.all()
@@ -41,3 +26,19 @@ class ModelViewSet(viewsets.ModelViewSet):
 class MarkViewSet(viewsets.ModelViewSet):
     queryset = EqMark.objects.all()
     serializer_class = MarkSerializer
+
+    @action(detail=False, url_path='select', methods=['post'])
+    def select(self, request):
+        print(request.data['parameters'])
+        data =json.loads(request.data['parameters'])
+        print(data)
+        wpq = int(data['wpq'])
+        wph = int(data['wph'])
+        eqtype_name = data['eqtype']
+        marks = EqMark.objects.filter(eqtype=EqType.objects.get(eqtype=eqtype_name))
+        choosen = choose_pumps(marks, (wpq, wph))
+        srl = MarkSerializer(choosen, many=True)
+        print('here')
+        # srl.data['success'] = True
+        r = Response(srl.data)
+        return Response(srl.data)
