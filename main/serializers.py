fb34a17c0b06eb7511e13a1364718a64de6ded86
Alex <alex_govr@mail.ru>
2021-01-31 10:55:46 +0300

    upd: srl: add related data

--- main/serializers.py
+++ main/serializers.py
@@ -10,18 +10,27 @@ class ManufacturerSerializer(serializers.ModelSerializer):
 
 
 class EqModelSerializer(serializers.ModelSerializer):
+
+    manufacturer = ManufacturerSerializer()
+
     class Meta:
         model = EqModel
         fields = ['id', 'eqmodel', 'manufacturer']
 
 
 class TypeSerializer(serializers.ModelSerializer):
+
+    eqmodel = EqModelSerializer()
+
     class Meta:
         model = EqType
         fields = ['id', 'eqtype', 'eqmodel', 'manufacturer']
 
 
 class MarkSerializer(serializers.ModelSerializer):
+
+    eqtype = TypeSerializer()
+
     class Meta:
         model = EqMark
         fields = ['id', 'eqmark', 'eqtype', 'eqmodel', 'manufacturer']
