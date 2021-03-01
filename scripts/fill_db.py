3ec456313eae6a6f8431b81bf0d5ec2b9174efdb
Alex <alex_govr@mail.ru>
2021-03-01 13:21:09 +0300

    upd: filling db script

--- scripts/fill_db.py
+++ scripts/fill_db.py
@@ -9,47 +9,88 @@ from django.core.exceptions import ObjectDoesNotExist
 from main.models import Manufacturer, EqMark, EqModel, EqType
 
 
-def run():
+class InstanceHandler:
 
-    manufacturer = 'grundfos'
-    eqmodel = 'CR'
-    eqtype = '1s'
-    manuf_inst = Manufacturer.objects.get(name = manufacturer)
+    def __init__(self):
+        self.info = {}
 
-    table = pd.read_excel('data/data.xlsx', sheet_name = manufacturer)
-    rows_total = table.count()[0]
-    points_count = 6
-
-    try:
-        model_inst = EqModel.objects.get(manufacturer = manuf_inst, eqmodel = eqmodel)
-    except ObjectDoesNotExist:
-        model_inst = EqModel(manufacturer = manuf_inst, eqmodel = eqmodel)
-        model_inst.save()
+    def findcreate(self, model, **kwargs):
+        key = model.__name__
+        try:
+            inst = model.objects.get(**kwargs)
+            self.__addinfo__(key, 'update')
+        except ObjectDoesNotExist:
+            inst = model(**kwargs)
+            inst.save()
+            self.__addinfo__(key, 'new')
+        return inst
     
-    try:
-        type_inst = EqType.objects.get(eqmodel = model_inst, eqtype = eqtype, manufacturer=manuf_inst)
-    except ObjectDoesNotExist:
-        type_inst = EqType(eqmodel = model_inst, eqtype = eqtype, manufacturer=manuf_inst)
-        type_inst.save()
+    def __addinfo__(self, key, action):
+        if key not in self.info:
+            self.info[key] = {
+                'new': 0,
+                'update': 0,
+            }
+        self.info[key][action] += 1
+
+
+def run():
+    handler = InstanceHandler()
+    manufacturers = ['grundfos', 'dp-pumps']
+    for manuf in manufacturers:
+        table = pd.read_excel('data/data.xlsx', sheet_name=manuf)
+        fill_from_ws(table, manuf, handler)
+
+    print('Updated succefully!')
+    print(*handler.info.items(), sep='\n')
+
+
+def fill_from_ws(table, manuf, handler):
+
+
+    manuf_inst = handler.findcreate(Manufacturer, name=manuf)
+    rows_total = table.count()[0]
 
     for row in range(rows_total):
+        eqmodel = table['model'][row]
+        eqtype = table['type'][row]
         eqmark = table['mark'][row]
-        q_points = [str(table[f'q{point}'][row]) for point in range(points_count)]
-        p2_points = [str(table[f'p{point}'][row]) for point in range(points_count)]
-        npsh_points = [str(table[f'npsh{point}'][row]) for point in range(points_count)]
-        efficiency_points = [str(table[f'eff{point}'][row]) for point in range(points_count)]
-        h_points = [str(table[f'h{point}'][row]) for point in range(points_count)]  
+        dn = table['dn'][row]
+        cost = table['cost'][row]
+        speed = table['speed'][row]
+        q_points = list_by_prefix('q', table, row)
+        p2_points = list_by_prefix('p', table, row)
+        npsh_points = list_by_prefix('npsh', table, row)
+        efficiency_points = list_by_prefix('eff', table, row)
+        h_points = list_by_prefix('h', table, row)
+        q_optimal = float(table['q_opt'][row])
 
-        try:
-            mark_inst = EqMark.objects.get(eqmark = eqmark, manufacturer = manuf_inst, eqtype = type_inst)
-        except ObjectDoesNotExist:
-            mark_inst = EqMark(eqmark = eqmark, manufacturer = manuf_inst, eqtype = type_inst, eqmodel=model_inst)
-        
+        model_inst = handler.findcreate(EqModel, eqmodel=eqmodel, manufacturer=manuf_inst)
+        type_inst = handler.findcreate(EqType, eqmodel=model_inst, 
+                                        eqtype=eqtype, 
+                                        manufacturer=manuf_inst)
+        mark_inst = handler.findcreate(EqMark, eqmark=eqmark, 
+                                        manufacturer=manuf_inst, 
+                                        eqtype=type_inst, 
+                                        eqmodel=model_inst)
+        # assign if new mark or change if existing one
         mark_inst.h_curve_points = ','.join(h_points)
         mark_inst.q_curve_points = ','.join(q_points)
         mark_inst.p2_curve_points = ','.join(p2_points)
         mark_inst.npsh_curve_points = ','.join(npsh_points)
         mark_inst.efficiency_curve_points = ','.join(efficiency_points)
+        mark_inst.q_optimal = q_optimal
+        mark_inst.dn=dn
+        mark_inst.cost=cost
+        mark_inst.speed=speed
         mark_inst.save()
 
-    print('Updated succefully!')
+def list_by_prefix(prefix, table, row):
+    i = 0
+    colname = f'{prefix}{i}'
+    ls = []
+    while colname in table:
+        ls.append(str(table[colname][row]))
+        i += 1
+        colname = f'{prefix}{i}'
+    return ls
